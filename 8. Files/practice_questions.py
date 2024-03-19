1703ce3a1fde783bdd0c796d60fe64ba8f0a355e
Michael Ward <mwardcoding@gmail.com>
2024-03-19 00:11:07 -0400

    practice questions and delete function in clipboard

---
+++ 8. Files/practice_questions.py
@@ -0,0 +1,26 @@
+# 1. What is a relative path relative to?
+    # the current working directory
+
+# 2. What does an absolute path start with?
+    #the root folder, / for mac, C:\ for windows
+
+# 3. What do the os.getcwd() and os.chdir() functions do?
+    #os.getcwd() gets the current working drive, os.chdir() changes the current working drive
+
+# 4. What are the . and .. folders?
+    # . is the current folder you're in, .. is the folder up one level from the current (parent)
+
+# 5. In C:\bacon\eggs\spam.txt, which part is the dir name, and which part is the base name?
+    # dir name is C:\bacon\eggs, the base name is spam.txt
+
+# 6. What are the three “mode” arguments that can be passed to the open() function?
+    # read, write, append
+
+# 7. What happens if an existing file is opened in write mode?
+    # the file is erased and completely overwritten
+
+# 8. What is the difference between the read() and readlines() methods?
+    # read() reads the entire file like a string, readlines() reads a file line by line
+
+# 9. What data structure does a shelf value resemble?
+    # a dictionary
