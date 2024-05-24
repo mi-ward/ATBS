fcbaf3f575026d998e0e7c17d18ac1cbc54212bc
Michael Ward <mwardcoding@gmail.com>
2024-05-23 22:21:57 -0400

    CSV and JSON

--- 15. CSV and JSON/getOpenWeather.py
+++ 15. CSV and JSON/getOpenWeather.py
@@ -5,25 +5,32 @@ import os, json, requests, sys
 
 APPID = os.environ['OPEN_WEATHER_APP_ID']
 
-
 # Compute location from command line arguments.
 
 if len(sys.argv) < 2:
-    print('Usage: getOpenWeather.py city_name, 2-letter_country_code')
+    print('Usage: getOpenWeather.py city_name, 2-letter_state_code, 2-letter_country_code')
     sys.exit()
-location = ' '.join(sys.argv[1:])
+    
+location = ','.join(sys.argv[1:])
 
-lat = 40.727090
-lon = -73.946730
+# Retrieve lat and lon from API``
+location_url = 'http://api.openweathermap.org/geo/1.0/direct?' \
+                'q=%s&appid=%s' \
+                % (location, APPID)
+                
+location_res = requests.get(location_url)
+location_res.raise_for_status()
+location = location_res.json()
+lat = location[0]['lat']
+lon = location[0]['lon']
+
 
 # Download the JSON data from OpenWeatherMap.org's API
-url = 'https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=%s' \
+url = 'https://api.openweathermap.org/data/2.5/forecast?lat=%s&lon=%s&appid=%s' \
        % (lat, lon, APPID)
        
 response = requests.get(url)
 response.raise_for_status()
-
-#http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 
 # Uncomment to see the raw JSON text:
 #print(response.text)
@@ -32,16 +39,14 @@ response.raise_for_status()
 weatherData = json.loads(response.text)
 
 # Print weather descriptions.
-w = weatherData
-print(w)
-import pprint
-pprint.pprint(w)
-# print('Current weather is in %s:' % (location))
-# print(w[0]['weather'][0]['main'], '-', w[0]['weather'][0]['description'])
-# print()
-# print('Tomorrow:')
-# print(w[1]['weather'][0]['main'], '-', w[1]['weather'][0]['description'])
-# print()
-# print('Day after tomorrow:')
-# print(w[2]['weather'][0]['main'], '-', w[2]['weather'][0]['description'])
+w = weatherData['list']
+
+print('Current weather is in %s, %s:' % (location[0]['name'], location[0]['state']))
+print(w[0]['weather'][0]['main'], '-', w[0]['weather'][0]['description'])
+print()
+print('Tomorrow:')
+print(w[1]['weather'][0]['main'], '-', w[1]['weather'][0]['description'])
+print()
+print('Day after tomorrow:')
+print(w[2]['weather'][0]['main'], '-', w[2]['weather'][0]['description'])
 
---
+++ 15. CSV and JSON/practice_questions.md
@@ -0,0 +1,21 @@
+# Practice Questions
+1. What are some features Excel spreadsheets have that CSV spread-sheets don’t?
+    - styling, charts, formatting, images, charts, you can adjust cell width and height
+
+2. What do you pass to csv.reader() and csv.writer() to create reader and writer objects?
+    - File objects obtained from open()
+
+3. What modes do File objects for reader and writer objects need to be opened in?
+    - 'rb' and 'wb' mode respectively
+
+4. What method takes a list argument and writes it to a CSV file?
+    - writerow()
+
+5. What do the delimiter and lineterminator keyword arguments do?
+    - delimiter changes the string to separate the cells, lineterminator changes the string to separate the rows
+
+6. What function takes a string of JSON data and returns a Python data structure?
+    - json.loads()
+
+7. What function takes a Python data structure and returns a string of JSON data?
+    - json.dumps()
